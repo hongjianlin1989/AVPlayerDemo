4a796437bf35e6ee1d12a15605d7d3ce1ae2a80e
hongjianlin1989 <ttzb1989@yahoo.com>
2022-10-19 14:47:52 -0400

    Initial Commit

---
+++ AVPlayerDemo/AVPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVPlayerDemoApp.swift
+//  AVPlayerDemo
+//
+//  Created by Hongjian Lin on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AVPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVPlayerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AVPlayerDemo
+//
+//  Created by Hongjian Lin on 10/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
